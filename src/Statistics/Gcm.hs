55991fc62737981cc764067f6fe298274732e52e
Dominik Schrempf <dominik.schrempf@gmail.com>
2020-06-07 18:32:14 +0200

    much faster now with lists (!)

--- src/Statistics/Gcm.hs
+++ src/Statistics/Gcm.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 {- |
 Module      :  Statistics.Gcm
@@ -20,24 +19,12 @@ For general help, please refer to the README distributed with the library.
 module Statistics.Gcm
   (
     gcm
+  , unsafeGcm
   , smooth
+  , unsafeSmooth
   ) where
 
--- TODO: Provide unsafe but faster functions.
-
 -- TODO: LCM.
-
--- TODO: Write down assumptions of functions.
-
--- TODO: All this is slow:
---
--- - snoc is used extensively
---
--- - vectors are manipulated extensively
---
--- - functions recompute the length repeatedly
---
--- Solution: Use mutable vectors and store indices.
 
 import qualified Data.Vector.Generic as V
 import qualified Data.Vector.Generic.Mutable as M
@@ -48,45 +35,32 @@ import Data.Vector.Generic (Vector)
 -- diff v = V.zipWith (-) (V.tail v) v
 -- {-# SPECIALIZE diff :: (Vector v Double) => v Double -> v Double #-}
 
--- TODO: THI TAKES 10 PER CENT OF THE TIME.
 slope :: (Real a, Real b) => a -> a -> b -> b -> Double
 slope x0 x1 y0 y1 = realToFrac (y1 - y0) / realToFrac (x1 - x0)
 {-# SPECIALIZE slope :: Int -> Int -> Double -> Double -> Double #-}
+{-# SPECIALIZE slope :: Double -> Double -> Double -> Double -> Double #-}
 {-# INLINE slope #-}
 
--- -- Slope at index.
--- slopeAt :: (Fractional a, Vector v a, Vector v Int) => Int -> v Int -> v a -> a
--- slopeAt i xs ys = slope (xs V.! (i-1)) (xs V.! i) (ys V.! (i-1)) (ys V.! i)
--- {-# SPECIALIZE slopeAt :: (Vector v Double, Vector v Int)
---                        => Int -> v Int -> v Double -> Double #-}
-
--- Slope at index.
-slopeAt :: (Real a, Real b, Vector v a, Vector v b) => Int -> v a -> v b -> Double
-slopeAt i xs ys = slope (xs V.! (i-1)) (xs V.! i) (ys V.! (i-1)) (ys V.! i)
-{-# SPECIALIZE slopeAt :: (Vector v Double, Vector v Int) => Int -> v Int -> v Double -> Double #-}
-
--- TODO: THIS TAKES 25 PERCENT OF THE TIME.
-rmSecondLast :: Vector v a => v a -> v a
-rmSecondLast v = V.slice 0 (l-2) v `V.snoc` V.last v
-  where l = V.length v
-{-# SPECIALIZE rmSecondLast :: (Vector v Double) => v Double -> v Double #-}
-
 -- Pool the last value in a vector until convexity is preserved.
-pool :: (Real a, Ord a, Vector v a, Real b, Vector v b, Vector v Double)
-     => v a -> v b -> v Double -> (v a, v b, v Double)
-pool xs ys ss | l <= 2    = (xs, ys, ss)
-              | otherwise = if s0 > s1 then (xs, ys, ss) else pool xs' ys' ss'
+pool :: (Ord a, Real a, Show a, Real b, Show b)
+     => [a] -> [b] -> [Double] -> a -> b -> ([a], [b], [Double])
+-- Points are ordered x0, x1, x2; and y0, y1, y2; s01 is slope from x0 to x1,
+-- and so on.
+pool xs@(x1:_) ys@(y1:_) ss@(s01:_) x2 y2 =
+  if s01 < s12
+  then (x2:xs, y2:ys, s12:ss)
+  else pool (tail xs) (tail ys) (tail ss) x2 y2
   where
-    l  = V.length xs
-    -- Slope of last element (length of ss is one lower than l).
-    s0 = ss V.! (l-2)
-    -- Slope of second last element (l >= 3, see guard above).
-    s1 = ss V.! (l-3)
-    xs' = rmSecondLast xs
-    ys' = rmSecondLast ys
-    ss' = V.init ss `V.snoc` slopeAt (l-2) xs' ys'
-{-# SPECIALIZE pool :: (Vector v Double, Vector v Int)
-                    => v Int -> v Double -> v Double -> (v Int, v Double, v Double) #-}
+    s12 = slope x1 x2 y1 y2
+-- Initialization and fallback if all points are removed during pooling.
+pool [x] [y] [] x' y' = (x':[x], y':[y], [slope x x' y y'])
+pool xs ys ss x y = error $ "pool: xs, ys, ss, x, y: "
+                    ++ show xs ++ ", " ++ show ys ++ ", " ++ show ss
+                    ++ ", " ++ show x ++ ", " ++ show y ++ "."
+{-# SPECIALIZE pool :: [Int] -> [Double] -> [Double] -> Int -> Double
+                    -> ([Int], [Double], [Double]) #-}
+{-# SPECIALIZE pool :: [Double] -> [Double] -> [Double] -> Double -> Double
+                    -> ([Double], [Double], [Double]) #-}
 
 -- Check if vector is ordered strictly (<).
 strictlyOrdered :: (Ord a, Vector v a, Vector v Bool) => v a -> Bool
@@ -94,30 +68,44 @@ strictlyOrdered xs | V.length xs <= 1 = True
                    | otherwise        = V.and $ V.zipWith (<) xs (V.tail xs)
 
 -- | Greatest convex minorant. Uses the Pool Adjacent Violators Algorithm
--- (PAVA). The predictors are have to be ordered with no ties.
+-- (PAVA). It is required that the predictors are ordered with no ties, and that
+-- the lengths of the vectors are equal.
 --
 -- Usage:
 --
 -- @
 --  gcm predictors responses = (indices, values, slopes)
 -- @
-gcm :: forall v a b . (Real a, Real b, Vector v a, Vector v b, Vector v Double, Vector v Int)
-    => v a -> v b -> (v a, v b, v Double)
--- TODO: Check order of ps.
-gcm ps rs | l == 0    = (V.empty, V.empty, V.empty)
-          | l == 1    = (ps, rs, V.empty)
-          | otherwise = go (V.take 1 ps, V.take 1 rs, V.singleton $ slopeAt 1 ps rs) (1 :: Int)
+--
+gcm :: (Real a, Real b, Show a, Vector v a, Show b, Vector v b, Vector v Bool)
+    => v a -> v b -> ([a], [b], [Double])
+gcm ps rs | lPs /= lRs = error $ "gcm: Number of predictors is " ++ show lPs
+                         ++ ", but number of responses is " ++ show lRs ++ "."
+          | not (strictlyOrdered ps) = error "gcm: The predictors are not strictly ordered."
+          | otherwise = unsafeGcm ps rs
+  where lPs = V.length ps
+        lRs = V.length rs
+
+-- | See 'gcm'.
+--
+-- Assume that:
+-- - the lengths of the provided vectors are equal;
+-- - the predictors are ordered.
+unsafeGcm :: (Real a, Real b, Show a, Vector v a, Show b, Vector v b)
+    => v a -> v b -> ([a], [b], [Double])
+unsafeGcm ps rs | l == 0    = ([], [], [])
+                | l == 1    = start
+                | otherwise = go start (1 :: Int)
   where
-    l  = V.length rs -- TODO: Error checking.
+    l     = V.length rs
+    start = ([V.head ps], [V.head rs], [])
     -- xs and ys: x and y values of gcm
-    -- i: next index of os
+    -- i: next index of rs
     go (xs, ys, ss) i | i >= l    = (xs, ys, ss)
-                      | otherwise = go (pool xs' ys' ss') (i+1)
+                      | otherwise = go (pool xs ys ss x' y') (i+1)
       where
-        -- TODO: Especially here, mutable vectors should be used.
-        xs' = xs `V.snoc` (ps V.! i)
-        ys' = ys `V.snoc` (rs V.! i)
-        ss' = ss `V.snoc` slopeAt (V.length xs' - 1) xs' ys'
+        x'  = ps V.! i
+        y'  = rs V.! i
 
 -- | Fill in missing values of an indexed vector.
 --
@@ -125,20 +113,29 @@ gcm ps rs | l == 0    = (V.empty, V.empty, V.empty)
 --  smooth [-2, 2, 4, 5] [0, 4, 10, 88] = [0, 1, 2, 3, 4, 7, 10, 88]
 -- @
 smooth :: (Vector v Bool, Vector v Double, Vector v Int) => v Int -> v Double -> v Double
-smooth xs ys | l == 0             = V.empty
-             | l == 1             = V.take 1 ys
-             | strictlyOrdered xs =
-               V.create (do zs <- M.new m
-                            go zs 0 1 (bounds 1)
-                            return zs)
-             | otherwise  = error "smooth: Index vector is not strictly ordered."
+smooth xs ys | V.length xs /= V.length ys =
+                 error "smooth: Index and value vector have different length."
+             | not (strictlyOrdered xs) =
+                 error "smooth: Index vector is not strictly ordered."
+             | otherwise = unsafeSmooth xs ys
+
+-- | See 'smooth'.
+--
+--
+-- Assume that:
+-- - the lengths of the provided vectors are equal;
+-- - the predictors are ordered.
+unsafeSmooth :: (Vector v Bool, Vector v Double, Vector v Int) => v Int -> v Double -> v Double
+unsafeSmooth xs ys | l == 0    = V.empty
+                   | l == 1    = V.take 1 ys
+                   | otherwise = V.create (do zs <- M.new m
+                                              go zs 0 1 (bounds 1)
+                                              return zs)
   where
+    l = V.length xs
     a = V.head xs
     b = V.last xs
     m = b - a + 1
-    l = if V.length xs == V.length ys
-        then V.length xs
-        else error "smooth: Index and value vector have different length."
     -- 0 <= i < m; index traversing resulting vector
     -- 0 <= j < l; index traversing given vectors
     bounds i = (xs V.! (i-1), xs V.! i, ys V.! (i-1), ys V.! i)

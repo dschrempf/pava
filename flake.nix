397c3f87f752a92e458a6f6abc409569c794dd05
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-09-02 15:20:40 +0200

    nix flake update

--- flake.nix
+++ flake.nix
@@ -5,29 +5,35 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          haskellPackages = pkgs.haskellPackages;
-          pava = haskellPackages.callCabal2nix "pava" self rec {};
-          pava-dev = pkgs.haskell.lib.doBenchmark pava;
-        in
-          {
-            packages.pava = pava;
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
+      flake-utils.lib.eachDefaultSystem (
+        system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            haskellPackages = pkgs.haskellPackages;
+            pava = haskellPackages.callCabal2nix "pava" self rec {};
+            pava-dev = pkgs.haskell.lib.doBenchmark pava;
+          in
+            {
+              packages.pava = pava;
 
-            defaultPackage = pava;
+              defaultPackage = pava;
 
-            devShell = pkgs.haskellPackages.shellFor {
-              packages = _: [ pava-dev ];
-              buildInputs = with pkgs; [
-                haskellPackages.cabal-install
-                haskellPackages.haskell-language-server
-                haskellPackages.stack
-              ];
-              doBenchmark = true;
-            };
-          }
-    );
+              devShell = pkgs.haskellPackages.shellFor {
+                packages = _: [ pava-dev ];
+                buildInputs = with pkgs; [
+                  bashInteractive
+                  haskellPackages.cabal-install
+                  haskellPackages.haskell-language-server
+                  haskellPackages.stack
+                ];
+                doBenchmark = true;
+                withHoogle = true;
+              };
+            }
+      );
 }
